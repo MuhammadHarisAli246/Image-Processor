d5b5b0212b6da7761d6bb01b16ef724a0ca5ed0d
MuhammadHarisAli246 <hafizharisali118@gmail.com>
2026-06-05 15:55:23 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+
+async function uploadImage() {
+
+    const file =
+    document.getElementById("imageInput").files[0];
+
+    const filter =
+    document.getElementById("filter").value;
+
+    const message =
+    document.getElementById("message");
+
+    const jobIdDiv =
+    document.getElementById("jobId");
+
+    if (!file) {
+        alert("Please select an image");
+        return;
+    }
+
+    try {
+
+        message.innerHTML = "Submitting Job...";
+
+        const response = await fetch(API_URL, {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                imageName: file.name,
+                filter: filter
+            })
+
+        });
+
+        const data = await response.json();
+
+        message.innerHTML = "Job Submitted Successfully";
+
+        jobIdDiv.innerHTML =
+            "Job ID: " + data.jobId;
+
+    }
+    catch (error) {
+
+        console.error(error);
+
+        message.innerHTML =
+            "Error submitting job";
+
+    }
+}
